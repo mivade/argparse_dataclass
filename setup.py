f41be08737403aa79722c46457d85c8db7c0241d
Michael V. DePalatis <mike@depalatis.net>
2023-01-21 08:03:46 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     url="https://github.com/mivade/argparse_dataclass",
 )
