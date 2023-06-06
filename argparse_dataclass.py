0d7e949fe60d4583758930e32810cf709d2972d5
Francis R. Kovacs <frankkovacs8@gmail.com>
2023-06-05 22:10:35 -0400

    wip migration to pyproject.toml

--- argparse_dataclass.py
+++ argparse_dataclass.py
@@ -287,8 +287,6 @@ else:
 # In Python 3.10, we can use types.NoneType
 NoneType = type(None)
 
-__version__ = "1.0.0"
-
 OptionsType = typing.TypeVar("OptionsType")
 ArgsType = typing.Optional[typing.Sequence[str]]
 
